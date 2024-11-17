ac2b89256eeb85bc3a37adc48d777551c59f3bbe
tejasambore <tejasambore2003@gmail.com>
2024-11-17 17:35:12 +0530

    Initial commit of adding random-password-generator project

---
+++ script.js
@@ -0,0 +1,27 @@
+const passBox = document.getElementById("password");
+const length = 8;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|{}[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPass() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passBox.value = password;
+}
+
+function copyPass() {
+  passBox.select();
+  document.execCommand("copy");
+}
